fbabb5efb9bb63538c3b6ef26256bdd9de678140
luciagore <lucy.gore@hotmail.co.uk>
2018-07-16 15:28:30 +0100

    Can print full statement to console

--- lib/statement.rb
+++ lib/statement.rb
@@ -12,4 +12,11 @@ class Statement
     puts "date || credit || debit || balance"
   end
 
+  def transaction_list(transactions = @account.transactions)
+    transactions.each do |entry|
+      puts "#{entry[:date]} || #{entry[:credit]} || #{entry[:debit]} || #{entry[:balance]}"
+    end
+
+  end
+
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -7,4 +7,10 @@ describe Statement do
     expect{ statement.table_title }.to output("date || credit || debit || balance\n").to_stdout
   end
 
+  it "prints each transaction from the account" do
+    account = double("account", :transactions => [{date: "16-07-2018", credit: 100, debit: nil, balance: 100}, {date: "16-07-2018", credit: nil, debit: 20, balance: 80}])
+    expect{ statement.transaction_list(account.transactions) }.to output(
+      "16-07-2018 || 100 ||  || 100\n16-07-2018 ||  || 20 || 80\n").to_stdout
+  end
+
 end
